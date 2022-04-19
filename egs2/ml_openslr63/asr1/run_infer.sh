1d08d9e99017bd9f3274e72075b74ef683afc721
rubenjohn1999 <rubenjohn1999@gmail.com>
2022-04-19 19:51:45 -0400

    Added data_prep scripts for mono

--- egs2/ml_openslr63/asr1/run_infer.sh
+++ egs2/ml_openslr63/asr1/run_infer.sh
@@ -12,16 +12,16 @@ mono_set="mono_ml"
 
 # Change mono_data.sh to data.sh before this
 
-./asr.sh \
-    --ngpu 1 \
-    --stage 1 \
-    --stop_stage 4 \
-    --speed_perturb_factors "0.9 1.0 1.1" \
-    --lang "ml" \
-    --train_set "${train_set}" \
-    --valid_set "${train_dev}" \
-    --test_sets "${mono_set}" \
-    --lm_train_text "data/${train_set}/text"
+#./asr.sh \
+#    --ngpu 1 \
+#    --stage 1 \
+#    --stop_stage 4 \
+#    --speed_perturb_factors "0.9 1.0 1.1" \
+#    --lang "ml" \
+#    --train_set "${train_set}" \
+#    --valid_set "${train_dev}" \
+#    --test_sets "${mono_set}" \
+#    --lm_train_text "data/${train_set}/text"
 
 ./asr.sh \
     --ngpu 1 \
