25a48cd3e29f25b00ef3cc2afe989ccf68ed7708
ukbharani <ukbharani@gmail.com>
2022-04-19 19:39:30 -0400

    Add file to parse output

---
+++ egs2/ml_openslr63/asr1/parse_output.py
@@ -0,0 +1,10 @@
+inference_file = open("exp/asr_train_asr_conformer_s3prlfrontend_hubert_fused_raw_ml_bpe150_sp/inference_lm_lm_train_lm_ml_bpe150_valid.loss.ave_asr_model_valid.acc.ave/mono_ml/text")
+output_file = open("output.tsv", "w")
+
+for line in inference_file:
+    values = line[5:]
+    result = values.split(" ")
+    file_name = result[0]
+    text = " ".join(result[1:])
+    output_file.write(file_name + "\t" + text)
+output_file.close()
