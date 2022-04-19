abd20c10785e037dd418a456559e1ed0c89aca30
ukbharani <ukbharani@gmail.com>
2022-04-19 15:41:41 -0400

    Add file to performe inference

--- egs2/ml_openslr63/asr1/inference.py
+++ egs2/ml_openslr63/asr1/inference.py
@@ -1,8 +1,21 @@
+from multiprocessing import parent_process
 import soundfile
 from espnet2.bin.asr_inference import Speech2Text
 import os
+
+# Download monolingual data
+# os.system("gdown --id 1xJX4--ahGgItJAoMnT99wrLAHDiUpaY6")
+os.system("gdown --id 1gEbggPorufsJTrXz7CGoQT3Jw6zBbZ4x")
+os.system("unzip -o monolingual_audio.zip")
+os.system("rm -f monolingual_audio.zip")
+
+parent_dir = "monolingual_audio/"
+for file in os.listdir(parent_dir):
+    os.system("fmpeg -i " + parent_dir + file + " -y -f wav -ar 16000 -ab 16 -ac 1 " 
+    + parent_dir + file)
+
 speech2text = Speech2Text.from_pretrained(
-    "rmampill/malayalam-asr",
+    "espnet/ml_openslr63",
     # Decoding parameters are not included in the model file
     maxlenratio=0.0,
     minlenratio=0.0,
@@ -14,10 +27,14 @@ speech2text = Speech2Text.from_pretrained(
 )
 # Confirm the sampling rate is equal to that of the training corpus.
 # If not, you need to resample the audio data before inputting to speech2text
+with open("monolingual_index.tsv", "w", encoding="utf-8") as output_file:
+    for file in os.listdir(parent_dir):
+
+        speech, rate = soundfile.read(parent_dir + file)
+        nbests = speech2text(speech)
+        text, *_ = nbests[0]
+        # output_file.write(file + "\t" + text + "\n")
+        # Write to tsv file
+        print(text)
 
 
-speech, rate = soundfile.read("speech.wav")
-nbests = speech2text(speech)
-
-text, *_ = nbests[0]
-print(text)
