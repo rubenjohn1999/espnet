4baf0a172e26754a078bc2b96f10b43a5fb03b5b
rubenjohn1999 <rubenjohn1999@gmail.com>
2022-04-19 16:57:28 -0400

    Added inference.py

--- egs2/ml_openslr63/asr1/run_infer.sh
+++ egs2/ml_openslr63/asr1/run_infer.sh
@@ -5,9 +5,18 @@ set -e
 set -u
 set -o pipefail
 
+train_set="train_ml"
+train_dev="dev_ml"
+test_set="test_ml"
+
 ./asr.sh \
     --ngpu 1 \
     --skip_data_prep true \
     --skip_train true \
     --asr_exp exp/asr_train_asr_conformer_s3prlfrontend_hubert_fused_raw_ml_bpe150_sp \
-    --lm_exp exp/lm_train_lm_ml_bpe150
+    --lm_exp exp/lm_train_lm_ml_bpe150 \
+    --feats_type raw \
+    --feats_normalize utt_mvn \
+    --train_set "${train_set}" \
+    --valid_set "${train_dev}" \
+    --test_sets "${test_set}"
