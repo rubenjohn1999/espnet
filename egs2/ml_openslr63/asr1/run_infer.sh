83d6ebc2316310b1a19b5e51d69c8a48bc2ce7d2
rubenjohn1999 <rubenjohn1999@gmail.com>
2022-04-19 20:13:52 -0400

    Added data_prep scripts for mono

--- egs2/ml_openslr63/asr1/run_infer.sh
+++ egs2/ml_openslr63/asr1/run_infer.sh
@@ -12,35 +12,36 @@ mono_set="mono_ml"
 
 # Change mono_data.sh to data.sh before this
 
+./asr.sh \
+    --ngpu 1 \
+    --stage 1 \
+    --stop_stage 4 \
+    --speed_perturb_factors "0.9 1.0 1.1" \
+    --lang "ml" \
+    --skip_train true \
+    --asr_exp exp/asr_train_asr_conformer_s3prlfrontend_hubert_fused_raw_ml_bpe150_sp \
+    --lm_exp exp/lm_train_lm_ml_bpe150 \
+    --feats_type raw \
+    --feats_normalize utt_mvn \
+    --train_set "${train_set}" \
+    --valid_set "${train_dev}" \
+    --test_sets "${test_set} ${mono_set}" \
+    --lm_train_text "data/${train_set}/text"
+
 #./asr.sh \
 #    --ngpu 1 \
-#    --stage 1 \
-#    --stop_stage 4 \
+#    --inference_nj 3 \
 #    --speed_perturb_factors "0.9 1.0 1.1" \
 #    --lang "ml" \
+#    --skip_data_prep true \
+#    --skip_train true \
+#    --asr_exp exp/asr_train_asr_conformer_s3prlfrontend_hubert_fused_raw_ml_bpe150_sp \
+#    --lm_exp exp/lm_train_lm_ml_bpe150 \
+#    --nbpe 150 \
+#    --feats_type raw \
+#    --feats_normalize utt_mvn \
 #    --train_set "${train_set}" \
 #    --valid_set "${train_dev}" \
 #    --test_sets "${mono_set}" \
 #    --lm_train_text "data/${train_set}/text"
 
-./asr.sh \
-    --ngpu 1 \
-    --inference_nj 3 \
-    --speed_perturb_factors "0.9 1.0 1.1" \
-    --lang "ml" \
-    --skip_data_prep true \
-    --skip_train true \
-    --asr_exp exp/asr_train_asr_conformer_s3prlfrontend_hubert_fused_raw_ml_bpe150_sp \
-    --lm_exp exp/lm_train_lm_ml_bpe150 \
-    --nbpe 150 \
-    --feats_type raw \
-    --feats_normalize utt_mvn \
-    --train_set "${train_set}" \
-    --valid_set "${train_dev}" \
-    --test_sets "${mono_set}" \
-    --lm_train_text "data/${train_set}/text"
-
-
-#    --stage 12 \
-#    --lang "ml" \
-#    --stop_stage 13 \
