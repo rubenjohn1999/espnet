285f0a1603b96e9fdee64eef790116fd90c54dc7
rubenjohn1999 <rubenjohn1999@gmail.com>
2022-04-21 18:18:51 -0400

    Edited conf

--- egs2/ml_openslr63/asr1/run.sh
+++ egs2/ml_openslr63/asr1/run.sh
@@ -17,6 +17,31 @@ lm_config=conf/train_lm.yaml
 ./asr.sh \
     --ngpu 1 \
     --stage 1 \
+    --stop_stage 10 \
+    --lang "ml" \
+    --use_lm true \
+    --lm_config "${lm_config}" \
+    --token_type bpe \
+    --nbpe 150 \
+    --bpemode "unigram" \
+    --feats_type raw \
+    --feats_normalize utt_mvn \
+    --speed_perturb_factors "0.9 1.0 1.1" \
+    --gpu_inference true \
+    --asr_config "${asr_config}" \
+    --inference_config "${inference_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${train_dev}" \
+    --test_sets "${train_dev} ${test_set}" \
+    --bpe_train_text "data/${train_set}/text" \
+    --lm_train_text "data/${train_set}/text"
+
+
+asr_config=conf/tuning/train_asr_conformer_s3prlfrontend_hubert_fused.yaml
+
+./asr.sh \
+    --ngpu 1 \
+    --stage 11 \
     --stop_stage 13 \
     --lang "ml" \
     --use_lm true \
@@ -25,6 +50,7 @@ lm_config=conf/train_lm.yaml
     --nbpe 150 \
     --bpemode "unigram" \
     --feats_type raw \
+    --feats_normalize utt_mvn \
     --speed_perturb_factors "0.9 1.0 1.1" \
     --gpu_inference true \
     --asr_config "${asr_config}" \
